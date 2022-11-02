062324d30d0ed73a82607cd26564ca0d99cfbafb
Larisa <lara_gil@mail.ru>
2022-11-02 23:09:27 +0500

    тесты прошли успешно, выгрузка в git

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,29 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        User u1 = new User("Вася1", "Пупкин1", (byte) 2);
+        User u2 = new User("Вася2", "Пупкин2", (byte) 24);
+        User u3 = new User("Вася3", "Пупкин3", (byte) 50);
+        User u4 = new User("Вася4", "Пупкин4", (byte) 84);
+        User[] users = {u1, u2, u3, u4};
+        for (User u: users) {
+            us.saveUser(u.getName(), u.getLastName(), u.getAge());
+            System.out.println("User с именем – " + u.getName() + " добавлен в базу данных");
+        }
+        List<User> allUsers = us.getAllUsers();
+        allUsers.forEach(System.out::println);
+        us.cleanUsersTable();
+        us.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,30 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getMySQLConnection() throws SQLException, ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "task113";
+        String userName = "root";
+        String password = "1234";
+        return getMySQLConnection(hostName, dbName, userName, password);
+    }
+
+    public static Connection getMySQLConnection(String hostName, String dbName,
+                                                String userName, String password) throws SQLException {
+
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(connectionURL, userName, password);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println(e.getMessage());
+        }
+        return conn;
+    }
 }
